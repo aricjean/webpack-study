7488d8b0065136bc545e7276d31c6ca5ad80ce88
reslie <1550167691@qq.com>
2019-11-17 01:12:35 +0800

    html-webpack-plugin 使用与教程区别

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+// clean-webpack-plugin 的使用与教程里面的可能有一些出处，可能是此包更新过使得使用方法改变了
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
