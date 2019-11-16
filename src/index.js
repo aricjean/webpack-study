2f2ec0bf2d5d84bd83873db8acd3d74465748edc
reslie <1550167691@qq.com>
2019-11-16 23:49:16 +0800

    配置更高级的 webpack entry 及 output 用法

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import _ from 'lodash';
-import './style.css'
+import './style.css';
+import printMe from './print.js';
+import Icon from './icon.png';
+
 function component(){
   let element = document.createElement('div');
+  let btn = document.createElement('button');
+
   element.innerHTML = _.join(['hello', 'webpack'], ' ');
   element.classList.add('hello');
-  
+
+  btn.innerHTML = 'Click me and check the console';
+  btn.onclick = printMe;
+
+  let myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+  element.appendChild(btn);
+
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
@@ -13,6 +16,12 @@ module.exports = {
         use: [
           'style-loader',
           'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)/,
+        use:[
+          'file-loader'
         ]
       }
     ]
